85d6ef7d0952820ce3b597d4c2907dd6561438d3
Josiah Voight <jv@Josiahs-MBP.lan>
2024-11-07 17:41:27 -0600

    remove img column

--- routers/dvds.py
+++ routers/dvds.py
@@ -22,7 +22,6 @@ class Dvd(BaseModel):
     id: int | None = None
     name: str
     status: str
-    img: str
     description: str
     genre: str
     rental_category: str
@@ -32,9 +31,9 @@ class Dvd(BaseModel):
 def create_dvd(dvd: Dvd):
     query = sa.text(f"""
         INSERT INTO dvds
-        (name, status, img, description, genre, rental_category)
+        (name, status,  description, genre, rental_category)
         VALUES
-        ('{dvd.name}','{dvd.status}','{dvd.img}','{dvd.description}','{dvd.genre}','{dvd.rental_category}')
+        ('{dvd.name}','{dvd.status}','{dvd.description}','{dvd.genre}','{dvd.rental_category}')
     """)
     engine = sa.create_engine("sqlite:///bcr.db")
     connection = engine.connect()
@@ -57,7 +56,6 @@ def update_dvd(dvd: Dvd):
         UPDATE dvds SET
             name = '{dvd.name}',
             status = '{dvd.status}',
-            img = '{dvd.img}',
             description = '{dvd.description}',
             genre = '{dvd.genre}',
             rental_category = '{dvd.rental_category}'
